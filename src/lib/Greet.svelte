0e54ee95937e097e90a6a0caf2872bfd0634a33d
Huw Percival <huwpercival@xmos.com>
2023-02-20 17:42:48 +0000

    initial svelte + rust app

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,22 @@
+
+<script>
+
+    import { invoke } from '@tauri-apps/api/tauri'
+
+    let name = ''
+    let message = 'Hi!'
+
+    async function greet() {
+        message = await invoke('greet', { name })
+    }
+
+</script>
+
+
+
+<input bind:value={name}/>
+<button on:click={greet}>
+get greeting
+</button>
+
+<p>{message}</p>
